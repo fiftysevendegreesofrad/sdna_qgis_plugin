20636956949240a408691a94b8f0581f754d5a6e
Crispin <cooperch@cf.ac.uk>
2015-12-07 15:23:11 +0000

    more parameter types in example

--- ExampleAlgorithm.py
+++ ExampleAlgorithm.py
@@ -23,14 +23,10 @@ __copyright__ = '(C) 2015, Cardiff University'
 
 __revision__ = '$Format:%H$'
 
-from PyQt4.QtCore import QSettings
-from qgis.core import QgsVectorFileWriter
-
 from processing.core.GeoAlgorithm import GeoAlgorithm
-from processing.core.parameters import ParameterVector
+from processing.core.parameters import *
 from processing.core.outputs import OutputVector
 from processing.tools import dataobjects, vector
-
 
 class ExampleAlgorithm(GeoAlgorithm):
     """This is an example algorithm that takes a vector layer and
@@ -61,12 +57,16 @@ class ExampleAlgorithm(GeoAlgorithm):
         self.name = 'Create copy of layer'
 
         # The branch of the toolbox under which the algorithm will appear
-        self.group = 'Algorithms for vector layers'
+        self.group = 'Spatial Design Network Analysis'
 
         # We add the input vector layer. It can have any kind of geometry
         # It is a mandatory (not optional) one, hence the False argument
         self.addParameter(ParameterVector(self.INPUT_LAYER,
             self.tr('Input layer'), [ParameterVector.VECTOR_TYPE_ANY], False))
+        self.addParameter(ParameterBoolean("boolvarname","boolean param",True))
+        # note ParameterSelection exists
+        self.addParameter(ParameterString("stringvarname","string param","default",False,True)) #multiline,optional
+        self.addParameter(ParameterTableField("fieldvarname","field",self.INPUT_LAYER,ParameterTableField.DATA_TYPE_NUMBER,True))#optional
 
         # We add a vector layer as output
         self.addOutput(OutputVector(self.OUTPUT_LAYER,
@@ -80,34 +80,4 @@ class ExampleAlgorithm(GeoAlgorithm):
         inputFilename = self.getParameterValue(self.INPUT_LAYER)
         output = self.getOutputValue(self.OUTPUT_LAYER)
 
-        # Input layers vales are always a string with its location.
-        # That string can be converted into a QGIS object (a
-        # QgsVectorLayer in this case) using the
-        # processing.getObjectFromUri() method.
-        vectorLayer = dataobjects.getObjectFromUri(inputFilename)
-
-        # And now we can process
-
-        # First we create the output layer. The output value entered by
-        # the user is a string containing a filename, so we can use it
-        # directly
-        settings = QSettings()
-        systemEncoding = settings.value('/UI/encoding', 'System')
-        provider = vectorLayer.dataProvider()
-        writer = QgsVectorFileWriter(output, systemEncoding,
-                                     provider.fields(),
-                                     provider.geometryType(), provider.crs())
-
-        # Now we take the features from input layer and add them to the
-        # output. Method features() returns an iterator, considering the
-        # selection that might exist in layer and the configuration that
-        # indicates should algorithm use only selected features or all
-        # of them
-        features = vector.features(vectorLayer)
-        for f in features:
-            writer.addFeature(f)
-
-        # There is nothing more to do here. We do not have to open the
-        # layer that we have created. The framework will take care of
-        # that, or will handle it if this algorithm is executed within
-        # a complex model
+        
