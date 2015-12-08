93c9296453524c25b0ee532be450b75fec825bc7
Crispin <cooperch@cf.ac.uk>
2015-12-08 15:39:45 +0000

    sdnaprovider now does all data io with dummy copy operation between

--- sDNAProvider.py
+++ sDNAProvider.py
@@ -23,77 +23,173 @@ __copyright__ = '(C) 2015, Cardiff University'
 
 __revision__ = '$Format:%H$'
 
+from PyQt4.QtGui import *
+import processing
 from processing.core.AlgorithmProvider import AlgorithmProvider
 from processing.core.ProcessingConfig import Setting, ProcessingConfig
-from sdna.ExampleAlgorithm import ExampleAlgorithm
 
+import os,sys
 
+from processing.core.GeoAlgorithm import GeoAlgorithm
+from processing.core.parameters import *
+from processing.core.outputs import OutputVector
+from processing.tools import dataobjects, vector, system
+from qgis.core import QgsVectorFileWriter
+
+try:
+    from Queue import Queue, Empty
+except ImportError:
+    from queue import Queue, Empty  
+from subprocess import PIPE, Popen, STDOUT
+
+sdna_to_qgis_vectortype = {"Polyline":ParameterVector.VECTOR_TYPE_LINE}
+sdna_to_qgis_fieldtype = {"Numeric":ParameterTableField.DATA_TYPE_NUMBER}
+
+def enqueue_output(command, queue):
+    queue.put(command+"\n\n")
+    ON_POSIX = 'posix' in sys.builtin_module_names
+    p = Popen(command+" 2>&1", shell=True, stdout=PIPE, bufsize=0, close_fds=ON_POSIX)
+    while True:
+        data = p.stdout.read(1)
+        if not data:
+            break
+        queue.put(data)
+    p.stdout.close()
+    p.wait()
+    queue.put("\nDone.\n")
+
+class SDNAAlgorithm(GeoAlgorithm):
+    
+    def __init__(self,sdnatool):
+        self.sdnatool = sdnatool
+        GeoAlgorithm.__init__(self)
+        
+    def defineCharacteristics(self):
+        # The name that the user will see in the toolbox
+        self.name = self.sdnatool.alias
+
+        # The branch of the toolbox under which the algorithm will appear
+        self.group = 'Spatial Design Network Analysis'
+
+        self.varnames = []
+        self.outputnames = []
+        for varname,displayname,datatype,filter,default,required in self.sdnatool.getInputSpec():
+            if datatype=="OFC":
+                self.outputnames+=[varname]
+            else:
+                self.varnames+=[varname]
+                
+            if datatype=="FC":
+                self.addParameter(ParameterVector(varname,self.tr(displayname),sdna_to_qgis_vectortype[filter],not required))
+            elif datatype=="OFC":
+                self.addOutput(OutputVector(varname,self.tr(displayname)))
+            elif datatype=="Field":
+                fieldtype,source = filter
+                self.addParameter(ParameterTableField(varname,self.tr(displayname),source,sdna_to_qgis_fieldtype[fieldtype],not required))
+            elif datatype=="Bool":
+                self.addParameter(ParameterBoolean(varname,self.tr(displayname),default))
+            elif datatype=="Text":
+                if filter:
+                    self.addParameter(ParameterSelection(varname,self.tr(displayname),filter))
+                else:
+                    self.addParameter(ParameterString(varname,self.tr(displayname),default,False,not required))
+            else:
+                assert False # unrecognized parameter type
+                
+
+    def processAlgorithm(self, progress):
+        args = {}
+        for outname,output in zip(self.outputnames,self.outputs):
+            args[outname]=output.getCompatibleFileName(self)
+        for vn in self.varnames:
+            args[vn]=self.getParameterValue(vn)
+        args["arcxytol"]=""
+        args["arcztol"]=""
+        
+        syntax = self.sdnatool.getSyntax(args)
+        
+        # convert inputs to shapefiles if necessary, renaming in syntax as appropriate
+        converted_inputs={}
+        for name,path in syntax["inputs"].iteritems():
+            if path:
+                tempfile = dataobjects.exportVectorLayer(processing.getObject(path))
+                progress.setInfo("exported "+path+" to "+tempfile)
+                converted_inputs[name]=tempfile
+        syntax["inputs"]=converted_inputs
+               
+        progress.setInfo(syntax.__repr__())    
+        
+        #working dummy copy:
+        src=syntax["inputs"].values()[0]
+        dst=syntax["outputs"].values()[0]
+        command = "copy "+src+" "+dst
+        progress.setInfo(command)
+        from shutil import copyfile
+        copyfile(src,dst)
+        copyfile(src[:-3]+"shx",dst[:-3]+"shx")
+        copyfile(src[:-3]+"dbf",dst[:-3]+"dbf")
+        #enqueue_output(command,Queue())
+        # run comand in process (change syntax so command is literal command line command)
+        
+        
 class sDNAProvider(AlgorithmProvider):
 
-    MY_DUMMY_SETTING = 'MY_DUMMY_SETTING'
+    def installsdna(self):
+        QMessageBox.critical(QDialog(),"sDNA: Error","Please install sDNA version 3.0 or later (http://www.cardiff.ac.uk/sdna/) then restart QGIS.")
 
+    def getSupportedOutputVectorLayerExtensions(self):
+        return ["shp"]
+        
     def __init__(self):
         AlgorithmProvider.__init__(self)
 
         # activate provider by default
         self.activate = True
+        
+        # find sDNA
+        matchstring = os.sep+"sDNA"+os.sep+"bin"
+        matches = [path for path in os.environ["PATH"].split(os.pathsep)
+                   if path[-len(matchstring):]==matchstring]
 
-        # Load algorithms
-        self.alglist = [ExampleAlgorithm()]
-        for alg in self.alglist:
-            alg.provider = self
+        if len(matches)==0:
+            altpath = "d:\\sdna\\arcscripts\\bin"
+            if os.path.exists(altpath):
+                sdnapath = altpath
+            else:
+                self.installsdna()
+                return
+        else:
+            sdnapath = matches[0]
+            if len(matches)>1:
+                QMessageBox.critical(QDialog(),"sDNA: Warning","Multiple sDNA installations found.  Using "+sdnapath)
+        sys.path.insert(0,sdnapath+os.sep+"..")
+        try:
+            from sDNAUISpec import get_tools
+        except ImportError:
+            self.installsdna()
+            return
+        
+        # load tools
+        self.alglist = []
+        for toolclass in get_tools():
+            qgistool = SDNAAlgorithm(toolclass())
+            qgistool.provider = self
+            self.alglist += [qgistool]
 
     def initializeSettings(self):
-        """In this method we add settings needed to configure our
-        provider.
-
-        Do not forget to call the parent method, since it takes care
-        or automatically adding a setting for activating or
-        deactivating the algorithms in the provider.
-        """
         AlgorithmProvider.initializeSettings(self)
-        # ProcessingConfig.addSetting(Setting('Example algorithms',
-            # ExampleAlgorithmProvider.MY_DUMMY_SETTING,
-            # 'Example setting', 'Default value'))
-
+        
     def unload(self):
-        """Setting should be removed here, so they do not appear anymore
-        when the plugin is unloaded.
-        """
         AlgorithmProvider.unload(self)
-        # ProcessingConfig.removeSetting(
-            # ExampleAlgorithmProvider.MY_DUMMY_SETTING)
-
+        
     def getName(self):
-        """This is the name that will appear on the toolbox group.
-
-        It is also used to create the command line name of all the
-        algorithms from this provider.
-        """
         return 'sDNA'
 
     def getDescription(self):
-        """This is the provider full name.
-        """
         return 'Spatial Design Network Analysis'
 
     def getIcon(self):
-        """We return the default icon.
-        """
         return AlgorithmProvider.getIcon(self)
 
     def _loadAlgorithms(self):
-        """Here we fill the list of algorithms in self.algs.
-
-        This method is called whenever the list of algorithms should
-        be updated. If the list of algorithms can change (for instance,
-        if it contains algorithms from user-defined scripts and a new
-        script might have been added), you should create the list again
-        here.
-
-        In this case, since the list is always the same, we assign from
-        the pre-made list. This assignment has to be done in this method
-        even if the list does not change, since the self.algs list is
-        cleared before calling this method.
-        """
         self.algs = self.alglist
